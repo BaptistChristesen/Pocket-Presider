7d46409b192b6c86aa82a279750bb3f25f10ffec
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-04-14 09:25:33 -0500

    Initial Commit

---
+++ Pocket P.O./Pocket_P_O_App.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_P_O_App.swift
+//  Pocket P.O.
+//
+//  Created by caden christesen on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_P_O_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
