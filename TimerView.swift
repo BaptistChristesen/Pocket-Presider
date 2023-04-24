a3ec50d29e0a8a521046ebc0d6c82bddf385c93b
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-04-24 09:25:07 -0500

    did more timer

--- TimerView.swift
+++ TimerView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct TimerView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
+}
+
+class TimerViewController: UIViewController {
+    @IBOutlet weak var timerLabel: UILabel!
+    var seconds = 60
+    var timer = Timer()
+    var isTimerRunning = false
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func pauseButtonTapped (sender: UIButton) {
+    }
+    @IBAction func resetButtonTapped (sender: UIButton) { }
+    override func viewDidLoad() {
+    }
+    super.viewDidLoad()
 }
 
 struct TimerView_Previews: PreviewProvider {
