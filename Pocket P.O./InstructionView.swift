6622c86d60eb94fcf1830ed94c34e12f026cffbb
Jakub <Wilk>
2023-05-12 09:26:06 -0500

    added picture and fixed instructions view

--- Pocket P.O./InstructionView.swift
+++ Pocket P.O./InstructionView.swift
@@ -11,17 +11,18 @@ import SwiftUI
         var body: some View {
             ZStack {
                 VStack {
-                    Text("Tips For P.O.").font(.title)
+                    Image("gavel2").resizable().frame(width: 150, height: 150)
+                    Text("Instructions & Tips").font(.title)
                     VStack (alignment: .leading) {
                         Text("Talk to people once you enter the chamber and try to get on their good side, give them the impression that you are a nice person")
                             .padding()
-                        Text("Pick the third session, because there is a greater chance that people will remember how well you did as PO. The third session is where all the uninteresting or unlikable bills are pushed into. This way, you are excused from having to make a speech that session.")
+                        Text("The chance that people will remember how well you did as PO is greater by choosing the third session. This is where all the uninteresting or unlikable bills are pushed to. This way, you are exempt from peforming a speech that session.")
                             .padding()
                         Text("In your speech, stress the fact that you will try your best to uphold fairness and speaking order as PO")
                             .padding()
                         Text("When selecting speakers and questioners, make it obvious that you are thoroughly looking around the room and being careful that you do not miss anybody. Some people are short and may be blocked by others who are taller. ")
                             .padding()
-                        Text("When selecting at random, before speaking order is established, spread out your selections across the room. Do not make it look like you are checking only one side of the room or the side closest to you. Some POs accidentally do this.")
+                        Text("When selecting at random, before speaking order is established, selections across the room. Do not make it look like you are checking only certain areas of the room. Some POs accidentally do this.")
                             .padding()
                     }
                     Spacer()
